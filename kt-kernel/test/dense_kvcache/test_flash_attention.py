a7b02fb78b2b0d67a935cc12b1dbae959468ea5c
Boxuan Yang <2335432902@qq.com>
2026-09-08 12:15:16 +0000

    [docs]: more test data

--- kt-kernel/test/dense_kvcache/test_flash_attention.py
+++ kt-kernel/test/dense_kvcache/test_flash_attention.py
@@ -22,9 +22,9 @@ head_dim = 128
 
 q_head_num_list = [32]
 block_len_list = [128]
-thread_num_list = [64]
-batch_size_list = [1, 32]
-sequence_length_list = [128, 4096]
+thread_num_list = [16, 32, 64]
+batch_size_list = [128]
+sequence_length_list = [4096]
 warmup_iterations = 2
 timing_iterations = 5
 test_mode_list = ["random"]
